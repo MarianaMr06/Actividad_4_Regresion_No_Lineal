445b968e48f93aac151730e1f19b3e962d5818a2
MarianaMr06 <A01735770@tec.mx>
2023-10-07 09:43:09 -0600

    AVANCE 1

---
+++ Regresion_No_Lineal.py
@@ -0,0 +1,33 @@
+#Cargamos librerias
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.special as special
+from scipy.optimize import curve_fit
+import seaborn as sns
+from sklearn.metrics import r2_score
+
+#Carga de Archivos
+credicel = pd.read_excel("Copia de df_limpio.xlsx")
+credicel.info()
+
+#Base sin edad negativa
+credicel_edad = credicel[credicel["edad_cliente"] >= 0]
+print(credicel_edad.info())
+
+#Definimos variables
+y_riesgo = credicel["riesgo"]
+y_score_buro_ed = credicel_edad["score_buro"]
+y_porc_eng = credicel["porc_eng"]
+y_limite_credito = credicel["limite_credito"]
+
+#riesgo
+    #score_buro
+
+#score_buro
+    #edad_cliente
+
+#porc_eng
+    #costo_total
+
+#
